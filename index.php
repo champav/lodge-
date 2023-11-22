5fe151a957b73e5fd2b6a1d570eedaa96c42842b
champav <136894369+champav@users.noreply.github.com>
2023-11-22 20:15:40 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,54 +1,29 @@
-<?php require_once('../config.php'); ?>
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
-<?php require_once('inc/header.php') ?>
-  <body class="layout-fixed layout-footer-fixed text-sm sidebar-mini control-sidebar-slide-open layout-navbar-fixed text-dark" data-new-gr-c-s-check-loaded="14.991.0" data-gr-ext-installed="" style="height: auto;">
-    <div class="wrapper">
+<?php require_once('config.php'); ?>
+ <?php require_once('inc/header.php') ?>
+  <body class="hold-transition layout-top-nav" >
+     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'portal';  ?>
      <?php require_once('inc/topBarNav.php') ?>
-     <?php require_once('inc/navigation.php') ?>
-              
-     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper bg-dark" style="min-height: 567.854px;">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 class="m-0"><?php echo ucwords(str_replace(array("/","_"), " ",$page)) ?></h1>
-              </div>
-              <!-- /.col -->
-              <!-- <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="./admin?<?php echo $page ?>"><?php echo ucwords(str_replace("_", " ",$page)) ?></a></li>
-                  <li class="breadcrumb-item active">Dashboard v1</li>
-                </ol>
-              </div> -->
-              <!-- /.col -->
-            </div>
-            <!-- /.row -->
-          </div>
-          <!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-        <!-- Main content -->
-        <section class="content  text-dark">
-          <div class="container-fluid">
-            <?php 
-              if(!file_exists($page.".php") && !is_dir($page)){
-                  include '404.html';
-              }else{
-                if(is_dir($page))
-                  include $page.'/index.php';
-                else
-                  include $page.'.php';
+     <?php 
+        if(!file_exists($page.".php") && !is_dir($page)){
+            include '404.html';
+        }else{
+          if(is_dir($page))
+            include $page.'/index.php';
+          else
+            include $page.'.php';
 
-              }
-            ?>
-          </div>
-        </section>
-        <!-- /.content -->
-  <div class="modal fade text-dark" id="confirm_modal" role='dialog'>
+        }
+      ?>
+       <script>
+        $(function(){
+          if($('header.masthead').lengt <= 0)
+            $('#mainNav').addClass('navbar-shrink')
+        })
+      </script>
+      <?php require_once('inc/footer.php') ?>
+      <div class="modal fade text-dark" id="confirm_modal" role='dialog'>
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -64,9 +39,9 @@
       </div>
     </div>
   </div>
-  <div class="modal fade text-dark" id="uni_modal" role='dialog'>
+  <div class="modal fade text-dark rounded-0" id="uni_modal" role='dialog'>
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
+      <div class="modal-content rounded-0">
         <div class="modal-header">
         <h5 class="modal-title"></h5>
       </div>
@@ -100,9 +75,6 @@
               <img src="" alt="">
       </div>
     </div>
-  </div>
-      </div>
-      <!-- /.content-wrapper -->
-      <?php require_once('inc/footer.php') ?>
+  </div> 
   </body>
 </html>
